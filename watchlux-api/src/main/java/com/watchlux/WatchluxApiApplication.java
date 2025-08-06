f5527dbcc2e0540f63046477ccbbcb8dec4286c4
Alex <alexismoreau37320@gmail.com>
2025-08-06 08:24:02 +0200

    Initial commit - projet WatchLuxe

---
+++ watchlux-api/src/main/java/com/watchlux/WatchluxApiApplication.java
@@ -0,0 +1,13 @@
+package com.watchlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchluxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchluxApiApplication.class, args);
+	}
+
+}
---
+++ watchlux-api/src/test/java/com/watchlux/WatchluxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchluxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
